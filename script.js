1116ba185d6d6e5d7277cbfeb70c96acdbe25c49
webploy99 <webploy99@gmail.com>
2024-05-24 14:10:06 +0700

    update commit

--- script.js
+++ script.js
@@ -30,6 +30,27 @@
     }
 
 
-    // ===============maquee slider=====================
+    // ===============slide show===================
+    document.addEventListener('DOMContentLoaded', function () {
+        var splide = new Splide('.splide', {
+            perPage: 1,
+            gap: '15px',
+            type: 'loop',
+            autoplay: true,
+            interval: 2000,
+            snap: true,
+            breakpoints: {
+                448: {
+                    perPage: 1
+                },
+                768: {
+                    perPage: 2
+                }
+            }
+            // start: 2,
+            // pagination: false,
+        });
+        splide.mount();
+    });
 
 
